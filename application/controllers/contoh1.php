8a62c0e872b40488a06b6ac0f97e14d7c1023212
zaaannn <david.fauzan41@gmail.com>
2023-09-25 16:33:24 +0700

    tambah data

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya David Fauzan
+            saya tinggal di daerah Bantul";
+    }
+}
